9bfddbc59f3f1e5bc110cfb96c3ff90f3f85ce0a
sacha <sacha.bloch.etu@univ-lille.fr>
2020-02-12 10:16:26 +0000

    :sparkles:feat(book-api.service): query the google books api

---
+++ adz-angular-stage/src/app/services/book-api/book-api.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import { ApiConfigService, BooksApiServiceConfig } from '../api-config/api-config.service'
+import {Book} from "../../models/book.model";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
+import {JsonToBook} from "../../converter/JsonToBook";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookApiService {
+
+  configBook: BooksApiServiceConfig;
+  private jsonToBook: JsonToBook;
+
+
+  constructor(private httpClient: HttpClient) {
+    this.configBook = ApiConfigService.configBook.booksService;
+    console.log(this.configBook);
+  }
+
+  public getListOfBooks(keywords: string) : Observable<Book[]>{
+
+    const params: HttpParams = new HttpParams()
+      .set('q', keywords)
+      .set('maxResults', String(this.configBook.maxItemsInResponse))
+      .set('key', this.configBook.apiKey);
+
+    return this.httpClient.get<Book[]>(this.configBook.apiUrl, {params})
+      .pipe(
+        map((books: any) => books.items ? this.convertJsonToBook(books.items) : [])
+      );
+
+  }
+
+  private convertJsonToBook(collection: any[]): Book[] {
+    return collection.map(item => this.jsonToBook.fromJson(item));
+  }
+
+}
