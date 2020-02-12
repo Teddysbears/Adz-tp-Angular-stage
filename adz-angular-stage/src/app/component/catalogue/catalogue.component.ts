b502fa7e6c2d1177100688576c7764d2631a162a
sacha <sacha.bloch.etu@univ-lille.fr>
2020-02-12 23:58:50 +0000

    :sparkles:feat(catalogue.component): catalogue css and book list

--- adz-angular-stage/src/app/component/catalogue/catalogue.component.ts
+++ adz-angular-stage/src/app/component/catalogue/catalogue.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import {BookApiService} from "../../services/book-api/book-api.service";
-import {Observable} from "rxjs";
 import {Book} from "../../models/book.model";
 
 @Component({
@@ -12,15 +11,14 @@ import {Book} from "../../models/book.model";
 export class CatalogueComponent implements OnInit {
 
   keywords: String;
-  booksList$: Observable<Book[]>;
+  booksList$: Book[];
 
 
   constructor(private activatedRoute: ActivatedRoute, private bookApiService: BookApiService) { }
 
   ngOnInit(): void {
     this.keywords = this.activatedRoute.snapshot.paramMap.get('keyword').replace(/\s/g, "+");
-    this.booksList$ = this.bookApiService.getListOfBooks(this.keywords.toString());
-    this.booksList$.forEach( book => console.log(book));
+    this.bookApiService.getListOfBooks(this.keywords.toString()).subscribe(res => this.booksList$ =res);
   }
 
 }
