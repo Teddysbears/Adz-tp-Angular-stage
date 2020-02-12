63ee1f3f53ad636dd5da072f4f52c2d55135c429
sacha <sacha.bloch.etu@univ-lille.fr>
2020-02-12 10:09:42 +0000

    :sparkles:feat(catalogue): Add catalogue page

---
+++ adz-angular-stage/src/app/component/catalogue/catalogue.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import {BookApiService} from "../../services/book-api/book-api.service";
+import {Observable} from "rxjs";
+import {Book} from "../../models/book.model";
+
+@Component({
+  selector: 'adz-catalogue',
+  templateUrl: './catalogue.component.html',
+  styleUrls: ['./catalogue.component.css']
+})
+export class CatalogueComponent implements OnInit {
+
+  keywords: String;
+  booksList$: Observable<Book>;
+
+
+  constructor(private activatedRoute: ActivatedRoute, private bookApiService: BookApiService) { }
+
+  ngOnInit(): void {
+    this.keywords = this.activatedRoute.snapshot.paramMap.get('keyword').replace(/\s/g, "+");
+    console.log(this.keywords);
+    this.bookApiService.getListOfBooks(this.keywords.toString());
+  }
+
+}
