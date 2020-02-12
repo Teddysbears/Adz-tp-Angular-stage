9a4b4842de6009afded19950cec8a1cbea90518b
sacha <sacha.bloch.etu@univ-lille.fr>
2020-02-12 10:14:56 +0000

    :sparkles:feat(JsonToBook): convert Json object to book object

---
+++ adz-angular-stage/src/app/converter/JsonToBook.ts
@@ -0,0 +1,20 @@
+import {Book} from "../models/book.model";
+
+export class JsonToBook {
+
+  fromJson(book: any): Book {
+
+    const fileds = book.volumeInfo;
+    const dateYear = new Date(fileds.publishedDate).getFullYear();
+
+    return new Book(
+      fileds.title || '',
+      fileds.subtitle || '',
+      (fileds.authors && fileds.authors.join(', ')) || '',
+      fileds.description || '',
+      isNaN(dateYear) ? '' : dateYear.toString(),
+      fileds.categories || '',
+      (fileds.imageLinks && fileds.imageLinks.smallThumbnail) || ''
+    );
+  }
+}
