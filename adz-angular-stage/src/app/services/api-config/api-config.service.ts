128146ba2afc77a57ce9e6fc7a81196e1c44067d
sacha <sacha.bloch.etu@univ-lille.fr>
2020-02-12 10:13:29 +0000

    :sparkles:feat(api-config.service): Get the configuration file for Google api

---
+++ adz-angular-stage/src/app/services/api-config/api-config.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+
+export interface BooksApiServiceConfig {
+  apiKey: string;
+  apiUrl: string;
+  maxItemsInResponse: number;
+  responseItems: string[];
+}
+
+export interface ConfigBook {
+  booksService: BooksApiServiceConfig
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiConfigService {
+
+  static configBook: ConfigBook;
+
+  constructor(private httpClient: HttpClient ){}
+
+  public getConfigBook() {
+    return new Promise((resolve, reject) => {
+      this.httpClient.get<ConfigBook>('assets/api-config.json')
+        .toPromise()
+        .then(data => resolve(data))
+        .catch(error => reject(error))
+    })
+      .then((config : ConfigBook) => {
+        ApiConfigService.configBook  = config;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+
+}
