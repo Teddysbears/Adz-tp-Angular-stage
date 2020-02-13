5c5d818b5733c433852b41ba70940eded5e7920a
sacha <sacha.bloch.etu@univ-lille.fr>
2020-02-13 12:05:05 +0000

    :lipstick:refactor(catalogue.component): responsive design

--- adz-angular-stage/src/app/component/catalogue/catalogue.component.ts
+++ adz-angular-stage/src/app/component/catalogue/catalogue.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import {BookApiService} from "../../services/book-api/book-api.service";
 import {Book} from "../../models/book.model";
+import { MatDialog } from "@angular/material/dialog";
+import {BookDialogOverviewComponent} from "../book-dialog-overview/book-dialog-overview.component";
 
 @Component({
   selector: 'adz-catalogue',
@@ -14,11 +16,22 @@ export class CatalogueComponent implements OnInit {
   booksList$: Book[];
 
 
-  constructor(private activatedRoute: ActivatedRoute, private bookApiService: BookApiService) { }
+  constructor(private activatedRoute: ActivatedRoute, private bookApiService: BookApiService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.keywords = this.activatedRoute.snapshot.paramMap.get('keyword').replace(/\s/g, "+");
     this.bookApiService.getListOfBooks(this.keywords.toString()).subscribe(res => this.booksList$ =res);
   }
 
+
+  openBookDialog(book : Book) :void {
+    const bookRef = this.dialog.open(BookDialogOverviewComponent, {
+      width: '80%',
+      data: book
+    });
+
+    bookRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
